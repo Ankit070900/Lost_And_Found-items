ce1b8535d2df4ca99d797c505e3eebd1d1bc0c49
Ankit Kumar <ankitmzn79@gmail.com>
2024-11-24 10:25:26 +0530

    modify home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,31 @@
 const Home = () => {
   return (
-    <div className="main_container">
-      <div className="heading">
-        <h1>Lost And Found</h1>
-        <h4>Lost it .List it . Find it .</h4>
+    <>
+      <div className="main_container">
+        <div className="heading">
+          <h1>Lost And Found</h1>
+          <h4>Lost it .List it . Find it .</h4>
+        </div>
+        <div className="front_img">
+          <img src="images\front.jpg" alt="" />
+        </div>
       </div>
-      <div className="front_img">
-        <img src="images\front.jpg" alt="" />
+      <div className="lost">
+        <div className="lost-img">
+          <img src="images\front.jpg" alt="" />
+        </div>
+        <div className="heading">
+          <h1>Lost Product</h1>
+          <h4>
+            Creating a "Lost and Found Product" website involves setting up two
+            main sections: Lost and Found. For the "Lost" page, you can design
+            it to allow users to submit details about their lost items and
+            search for them. Here's a detailed guide for structuring the Lost
+            page:
+          </h4>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
